ff6cec294ea2239799e09bd64d9b71b210b06247
ashisins <ashisins@ASHISINS-IN.in.oracle.com>
2019-07-15 19:55:09 +0530

    Oracle Database Configured

--- forward_port/forward_port/settings.py
+++ forward_port/forward_port/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'forward_port.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'orcl',
+        'USER': 'xref_user',
+        'PASSWORD': 'xref123',
+        'HOST': 'localhost',
+        'PORT': '1521',
     }
 }
 
